9523ba2943567568a66acc140e5efedc20d165a4
LenaPradoP <lena@atajo.es>
2024-12-11 21:38:51 +0100

    Aplico constantes y corrijo precio minutos extra

--- 1.2-Nivell2.php
+++ 1.2-Nivell2.php
@@ -1,27 +1,30 @@
 <?php 
 declare(strict_types = 1);
 
-echo "S1.T2.N2 Ejercicio 1" .PHP_EOL;
+echo "S1.T2.N2 Ejercicio 1" .PHP_EOL .PHP_EOL;
 
     $minutes = 7;
 
-    echo "El coste de la llamada es de " . calculateCost($minutes) . "€.".PHP_EOL;
+    echo "El coste de la llamada es de " . calculateCost($minutes) . "€.".PHP_EOL .PHP_EOL;
 
     function calculateCost(int $minutes) :  float {
-        $cost = 0.0;
+        define('INITIAL_COST', 0.10);
+        define('EXTRA_MINUTE_COST', 0.05);
 
         if ($minutes <= 3) {
-            $cost = 0.10;
+            $cost = INITIAL_COST;
         } else {
-            $cost = 0.10;
-            for ($i = 3 ; $i < $minutes ; $i++) {
-                $cost += 0.5;
+            $cost = INITIAL_COST;
+            $cost += ($minutes - 3) * EXTRA_MINUTE_COST;
             }
-        }
 
         return $cost;
     }
-    echo "S1.T2.N2 Ejercicio 2" .PHP_EOL;
+    echo "S1.T2.N2 Ejercicio 2" .PHP_EOL .PHP_EOL;
+        define('PRICE_CHOCOLATE', 1.00);
+        define('PRICE_GUM', 0.50);
+        define('PRICE_CARAMEL', 1.50);
+
         $chocolate = 3;
         $gum = 5;
         $caramel = 2;
@@ -33,20 +36,19 @@ echo "S1.T2.N2 Ejercicio 1" .PHP_EOL;
             . calculateTotal($chocolate, $gum, $caramel).PHP_EOL;
 
         function calculateChocolate(int $chocolate) : float {
-            return $chocolate * 1;
+            return $chocolate * PRICE_CHOCOLATE;
         }
     
         function calculateGum(int $gum) : float {
-            return $gum * 0.5;
+            return $gum * PRICE_GUM;
         }
 
         function calculateCaramel(int $caramel) : float {
-            return $caramel * 1.5;
+            return $caramel * PRICE_CARAMEL;
         }
 
         function calculateTotal(int $chocolate, int $gum, int $caramel) : float {
             return calculateChocolate($chocolate) + calculateGum($gum) + calculateCaramel($caramel);
         }
-
 
 ?>

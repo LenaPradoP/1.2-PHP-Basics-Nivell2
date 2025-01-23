a964434468c1911e7098633d6fb34304e483d2c8
LenaPradoP <lena@atajo.es>
2025-01-23 10:42:54 +0100

    Correciones después de peer review

--- 1.2-Nivell2.php
+++ 1.2-Nivell2.php
@@ -3,52 +3,60 @@ declare(strict_types = 1);
 
 echo "S1.T2.N2 Ejercicio 1" .PHP_EOL .PHP_EOL;
 
-    $minutes = 7;
+echo calculateCost(5);
 
-    echo "El coste de la llamada es de " . calculateCost($minutes) . "€.".PHP_EOL .PHP_EOL;
-
-    function calculateCost(int $minutes) :  float {
-        define('INITIAL_COST', 0.10);
-        define('EXTRA_MINUTE_COST', 0.05);
-
-        if ($minutes <= 3) {
-            $cost = INITIAL_COST;
-        } else {
-            $cost = INITIAL_COST;
-            $cost += ($minutes - 3) * EXTRA_MINUTE_COST;
-            }
-
-        return $cost;
-    }
-    echo "S1.T2.N2 Ejercicio 2" .PHP_EOL .PHP_EOL;
-        define('PRICE_CHOCOLATE', 1.00);
-        define('PRICE_GUM', 0.50);
-        define('PRICE_CARAMEL', 1.50);
-
-        $chocolate = 3;
-        $gum = 5;
-        $caramel = 2;
-
-        echo "El total de la compra es de: ".PHP_EOL 
-            . calculateChocolate($chocolate) . " + " 
-            . calculateGum($gum) . " + "
-            . calculateCaramel($caramel) . " = "
-            . calculateTotal($chocolate, $gum, $caramel).PHP_EOL;
-
-        function calculateChocolate(int $chocolate) : float {
-            return $chocolate * PRICE_CHOCOLATE;
-        }
+function calculateCost(float $callTime) : string {
+    define('MINIMUN_TIME', 3);
+    define('STARTING_COST', 0.10);
+    define('EXTRA_MINUTE_COST', 0.05);
     
-        function calculateGum(int $gum) : float {
-            return $gum * PRICE_GUM;
-        }
+    if ($callTime <= 0) {
+        return "El tiempo de llamada no ha sido registrado".PHP_EOL ;
+    } else if ($callTime <= 3) {
+        $finalCost = STARTING_COST;
+    } else {
+        $finalCost = STARTING_COST + ($callTime - MINIMUN_TIME) * EXTRA_MINUTE_COST;
+    }
+        return "El coste de la llamada es $finalCost" . '€.'.PHP_EOL .PHP_EOL ;
+}
 
-        function calculateCaramel(int $caramel) : float {
-            return $caramel * PRICE_CARAMEL;
-        }
+echo "S1.T2.N2 Ejercicio 2" .PHP_EOL .PHP_EOL;
 
-        function calculateTotal(int $chocolate, int $gum, int $caramel) : float {
-            return calculateChocolate($chocolate) + calculateGum($gum) + calculateCaramel($caramel);
-        }
+define('CHOCOLATE_PRICE', 1);
+define('GUM_PRICE', 0.5);
+define('CANDY_PRICE', 1.5);
+
+echo calculateTotal(calculateChocolate(5), calculateGum(3), calculateCandy(1));
+echo calculateTotal(calculateChocolate(5), calculateCandy(1));
+
+function calculateChocolate(int $chocolateAmount) : float {
+    $chocolateCost = $chocolateAmount * CHOCOLATE_PRICE;
+
+    echo "Chocolate $chocolateAmount x " . CHOCOLATE_PRICE . "€ = $chocolateCost" . '€' .PHP_EOL;
+
+    return $chocolateCost;
+}
+
+function calculateGum(int $gumAmount) : float {
+    $gumCost = $gumAmount * GUM_PRICE;
+    
+    echo "Chicle $gumAmount x " . GUM_PRICE . "€ = $gumCost" . '€' .PHP_EOL;
+
+    return $gumCost;
+}
+
+function calculateCandy(int $candyAmount) : float {
+    $candyCost = $candyAmount * CANDY_PRICE;
+
+    echo "Caramelo $candyAmount x " . CANDY_PRICE . "€ = $candyCost" . '€' .PHP_EOL;
+
+    return $candyCost;
+}
+
+function calculateTotal(float $chocolateCost = 0, float $gumCost = 0, float $candyCost = 0) : string {
+    $totalCost = $chocolateCost + $gumCost + $candyCost;
+
+    return "Precio total = $totalCost" .'€' .PHP_EOL .PHP_EOL;
+}
 
 ?>
